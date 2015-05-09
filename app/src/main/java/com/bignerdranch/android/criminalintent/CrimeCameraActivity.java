98b0f20146f8d9a26e546708b666b199b7a10e9b
Vahe <vahehar4@mail.ru>
2015-05-09 17:45:09 +0400

    now can take photos

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraActivity.java
@@ -1,11 +1,23 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Tigran on 5/8/2015.
  */
 public class CrimeCameraActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
